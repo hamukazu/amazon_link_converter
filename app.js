bbf310a7075e0c7c36cb23c9f65bb6e7d9c84914
kimikazu Kato <kimikazu@gmail.com>
2018-10-13 01:01:18 +0900

    First commit Changes to be committed: new file:   app.js new file:   index.html

---
+++ app.js
@@ -0,0 +1,45 @@
+"use strict"
+const assoc_id="hamukazu-22"
+
+function get_url(prod_id, assoc_id) {
+  return "https://www.amazon.co.jp/exec/obidos/isbn/"+prod_id+"/"+assoc_id;
+}
+
+function get_prod_id(url) {
+  const tags=["gp","dp","isbn","asin"];
+  const s=url.split("/");
+  for (var i=0; i<s.length-1; ++i) {
+    if (tags.includes(s[i])) {
+      if (s[i+1]=="product" && i+2<s.length) {
+        return s[i+2];
+      } else {
+        return s[i+1];
+      }
+    }
+  }
+}
+
+function convert_link(url, assoc_id) {
+  const prod_id=get_prod_id(url);
+  return get_url(prod_id, assoc_id);
+}
+
+new Vue({
+    el: "#top",
+    data: {
+        prod: "",
+        url: ""
+    },
+    methods: {
+        textchanged() {
+            if ( this.prod.indexOf("/")<0 ) {
+                this.url=get_url(this.prod, assoc_id);
+            } else {
+                this.url=convert_link(this.prod, assoc_id);
+            }
+        }
+    }
+});
+
+                              
+                              
